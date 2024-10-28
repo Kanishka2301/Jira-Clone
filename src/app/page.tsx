13d0d954993e72753ff754e7298ccf398e47f457
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-10-28 19:23:12 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="">
-      <Button>Primary</Button>
+      <Button size="xs">Primary</Button>
       <Button variant="secondary">Secondary</Button>
       <Button variant="destructive">Destructive</Button>
       <Button variant="ghost">Ghost</Button>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,7 +9,7 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default:
+        primary:
           "bg-gradient-to-b from-blue-600 to-blue-700 text-primary-foreground hover:from-blue-700 hover:to-blue-700",
         destructive:
           "bg-gradient-to-b from-amber-600 to-amber-700 text-destructive-foreground hover:from-amber-700 hover:to-amber-700",
@@ -23,14 +23,15 @@ const buttonVariants = cva(
           "bg-blue-100 text-blue-600 border-transparent hover:bg-blue-200 shadow-none",
       },
       size: {
-        default: "h-9 px-4 py-2",
-        sm: "h-8 rounded-md px-3 text-xs",
-        lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        default: "h-10 px-4 py-2",
+        sm: "h-8 rounded-md px-3",
+        lg: "h-12 rounded-md px-8",
+        xs: "h-7 rounded-md px-2 text-xs",
+        icon: "h-8 w-8",
       },
     },
     defaultVariants: {
-      variant: "default",
+      variant: "primary",
       size: "default",
     },
   }
