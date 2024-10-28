ee1e07cf24c56c4ed365846429a935ac78031746
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-10-28 22:34:49 +0530

    initial commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 interface AuthLayoutProps {
@@ -7,7 +8,17 @@ interface AuthLayoutProps {
 const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
     <main className="bg-neutral-100 min-h-screen">
-      <div className="mx-auto max-w-screen-2x p-4">{children}</div>
+      <div className="mx-auto max-w-screen-2x p-4">
+        <nav className="flex justify-between items-center">
+          <Image src="/logo.svg" alt="logo" width={152} height={56} />
+          <div className="flex items-center gap-2">
+            <Button variant="secondary">Sign Up</Button>
+          </div>
+        </nav>
+        <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
+          {children}
+        </div>
+      </div>
     </main>
   );
 };
