3cf32b3d8ebafed968ab2062da4caa4548da53a2
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-10-28 22:07:10 +0530

    initial commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,15 @@
+import Image from "next/image";
+
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <Image src="/logo.svg" height={50} width={100} alt="Logo" />
+      {children}
+    </div>
+  );
 };
 export default AuthLayout;
