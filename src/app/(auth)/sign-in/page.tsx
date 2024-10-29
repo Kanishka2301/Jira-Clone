f5d4d56b9adc2584ba86d5c6d3a6e7d9f23f9b68
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-10-29 19:09:22 +0530

    initial commit

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,4 +1,5 @@
+import { SignInCard } from "@/features/auth/components/sign-in-card";
 const SignInPage = () => {
-  return <div>Sign in page</div>;
+  return <SignInCard />;
 };
 export default SignInPage;
