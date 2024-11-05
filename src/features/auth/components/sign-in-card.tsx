e2521b77d8cc2c2cbf755b8318f993f6fb2a8c20
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-11-05 14:41:30 +0530

    initial commit

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-
 export const SignInCard = () => {
-  const [email, setEmail] = useState("");
-
   return (
     <Card className="w-full h-full md:w-[487px] border-none shadow-none">
       <CardHeader className="flex items-center justify-center text-center p-7">
@@ -19,12 +18,48 @@ export const SignInCard = () => {
           <Input
             required
             type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
+            value={""}
+            onChange={() => {}}
             placeholder="Enter email address"
             disabled={false}
           />
+          <Input
+            required
+            type="password"
+            value={""}
+            onChange={() => {}}
+            placeholder="Enter password"
+            disabled={false}
+            min={8}
+            max={56}
+          />
+          <Button disabled={false} size="lg" className="w-full">
+            Login
+          </Button>
         </form>
+      </CardContent>
+      <div className="px-7">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          <FcGoogle className="mr-2 size-5" />
+          Login with Google
+        </Button>
+        <Button
+          disabled={false}
+          variant="secondary"
+          size="lg"
+          className="w-full"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with GitHub
+        </Button>
       </CardContent>
     </Card>
   );
