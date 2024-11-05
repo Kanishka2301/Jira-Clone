6f9a331c7ce53ce17492a8fd573ec6997618198e
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-11-05 20:39:52 +0530

    initial commit

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,18 +1,28 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
 
 interface AuthLayoutProps {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: AuthLayoutProps) => {
+  const pathname = usePathname();
+  const isSignIn = pathname === "/sign-in";
+
   return (
     <main className="bg-neutral-100 min-h-screen">
       <div className="mx-auto max-w-screen-2x p-4">
         <nav className="flex justify-between items-center">
           <Image src="/logo.svg" alt="logo" width={152} height={56} />
           <div className="flex items-center gap-2">
-            <Button variant="secondary">Sign Up</Button>
+            <Button asChild variant="secondary">
+              <Link href={pathname === "/sign-in" ? "/sign-up" : "/sign-in"}>
+                {pathname === "/sign-in" ? "Sign Up" : "Login"}
+              </Link>
+            </Button>
           </div>
         </nav>
         <div className="flex flex-col items-center justify-center pt-4 md:pt-14">
