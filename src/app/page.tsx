2d2c77e63b249f31c27dc28d7f46c146cccbaeeb
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-11-11 18:48:12 +0530

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { createAdminClient } from "@/lib/appwrite";
 export default function Home() {
+  console.log(createAdminClient, "hello");
+
   return (
     <div className="">
       <Input />
