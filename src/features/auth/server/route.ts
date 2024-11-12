7a7e8b7a94330259828af4b5cc520888ab2905d2
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-11-12 21:46:07 +0530

    initial commit

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -9,6 +9,10 @@ import { AUTH_COOKIE } from "../constants";
 import { sessionMiddleware } from "@/lib/session-middleware";
 
 const app = new Hono()
+  .get("/current", sessionMiddleware, (c) => {
+    const user = c.get("user");
+    return c.json({ data: user });
+  })
   .post("/login", zValidator("json", loginSchema), async (c) => {
     const { email, password } = c.req.valid("json");
     const { account } = await createAdminClient();
@@ -42,9 +46,10 @@ const app = new Hono()
 
     return c.json({ success: true });
   })
-  .post("/layout", sessionMiddleware, (c) => {
+  .post("/logout", sessionMiddleware, async (c) => {
     const account = c.get("account");
     deleteCookie(c, AUTH_COOKIE);
+    await account.deleteSession("current");
     return c.json({ success: true });
   });
 
