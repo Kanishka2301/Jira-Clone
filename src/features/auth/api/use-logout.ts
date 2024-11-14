8fc395c2fea76b3c881fde881c55ba65abf8840c
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-11-14 22:07:58 +0530

    initial commit

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,14 +1,19 @@
-import { useMutation } from "@tanstack/react-query";
+import { QueryClient } from "./../../../../node_modules/@tanstack/query-core/src/queryClient";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { client } from "@/lib/rpc";
 import { InferRequestType, InferResponseType } from "hono/client";
 
 type ResponseType = InferResponseType<(typeof client.api.auth.logout)["$post"]>;
 
 export const useLogout = () => {
+  const QueryClient = useQueryClient();
   const mutation = useMutation<ResponseType, Error>({
     mutationFn: async () => {
       const response = await client.api.auth.logout["$post"]();
       return await response.json();
+    },
+    onSuccess: () => {
+      QueryClient.invalidateQueries({ queryKey: ["current"] });
     },
   });
   return mutation;
