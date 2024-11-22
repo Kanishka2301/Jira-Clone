4d645729ece4f91b58de520f54c3c2b71b4906fe
Kanishka2301 <kanishkaskumar2004@gmail.com>
2024-11-22 09:55:05 +0530

    initial commit

--- src/features/auth/components/user-button.tsx
+++ src/features/auth/components/user-button.tsx
@@ -1,6 +1,6 @@
 "use client";
-import { Loader } from "lucide-react";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Loader, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -12,6 +12,7 @@ import { useLogout } from "../api/use-logout";
 import { useCurrent } from "../api/use-current";
 
 export const UserButton = () => {
+  const { mutate: logout } = useLogout();
   const { data: user, isLoading } = useCurrent();
   if (isLoading) {
     return (
@@ -45,12 +46,26 @@ export const UserButton = () => {
         sideOffset={10}
       >
         <div className="flex flex-col items-center justify-center gap-2 px-2.5 py-4">
-          <Avatar className="size-10 hover:opacity-75 transition border border-neutral-300">
-            <AvatarFallback className="bg-neutral-200 font-medium text-neutral-500 flex items-center justify-center">
+          <Avatar className="size-[52px] border-neutral-300">
+            <AvatarFallback className="bg-neutral-200 text-xl font-medium text-neutral-500 flex items-center justify-center">
               {avatarFallback}
             </AvatarFallback>
           </Avatar>
+          <div className="flex flex-col items-center justify-center">
+            <p className="text-sm font-medium text-neutral-900">
+              {name || "User"}
+            </p>
+            <p className="text-xs text-neutral-500">{email}</p>
+          </div>
         </div>
+        <DottedSeparator className="mb-1" />
+        <DropdownMenuItem
+          onClick={() => logout()}
+          className="h-10 flex items-center justify-center text-amber-700 font-medium cursor-pointer"
+        >
+          <LogOut className="size-4 mr-2" />
+          Log out
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
